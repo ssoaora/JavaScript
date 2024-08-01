02efa9a24fbce8c1fdaf857c27490ec1afe013db
ssoaora <ssoaora@gmail.com>
2024-08-01 16:37:24 +0900

    ✨ Added: chap01/sec05 function practice

---
+++ chap01/sec05/function-practice01.js
@@ -0,0 +1,14 @@
+function hello(name) {
+  console.log('Hello, ' + name + '!');
+}
+
+hello('Andrew');
+
+
+
+// with template literals
+function hello2(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+hello2('Andy');
